ae245117f120ad785a60ef16eb5451cfb0420d9c
patztorre <85425508+patztorre@users.noreply.github.com>
2021-07-22 07:22:33 +0800

    practice complete

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,10 +1,18 @@
-import React from "react";
+import React, { useState } from "react";
 
 function App() {
+  setInterval(getTime, 1000);
+
+  let now = new Date().toLocaleTimeString();
+  const [time, setTime] = useState(now);
+
+  function getTime() {
+    setTime(new Date().toLocaleTimeString("it-IT"));
+  }
   return (
     <div className="container">
-      <h1>TIME</h1>
-      <button>Get Time</button>
+      <h1>{time}</h1>
+      <button onClick={getTime}>Get Time</button>
     </div>
   );
 }
